21561496bc6f82e27591aa5dc0c65c4deab9bd4b
Adam Huss <adamhuss13@gmail.com>
2024-03-22 14:06:58 -0700

    copied the rest of my code and added a uuid to new notes

--- Develop/server.js
+++ Develop/server.js
@@ -2,16 +2,41 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const PORT = 3000;
+const fs = require('fs')
+const {v4 : uuidv4} = require('uuid')
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+app.use(express.static('public')) // Middleware
 
 app.get('/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-})
-
-app.get('/', function (req, res) { // UPDATE to *
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+});
 
 app.get('/api/notes', function (req, res) {
-    res.sendFile(path.join(__dirname, './db/db.json'))
-})
+  res.sendFile(path.join(__dirname, './db/db.json'))
+});
 
+app.post('/api/notes', function (req, res) {
+  const newNote = req.body
+  console.log(newNote)
+  fs.readFile("./db/db.json", "utf-8", function(err, result){
+    let notes = JSON.parse(result)
+    newNote.id = uuidv4()
+
+    notes.push(newNote)
+    fs.writeFile("./db/db.json", JSON.stringify(notes), function(err, result){
+      console.log('New note has been added')
+    })
+  })
+  // Log request to the terminal
+  console.info(`${req.method} request received in terminal`)
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+});
+
+app.listen(PORT, () =>
+console.log(`Example app listening at http://localhost:${PORT}`)
+);
