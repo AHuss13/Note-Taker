7a966ad051dc2e91c16048da67983a915f523584
Adam Huss <adamhuss13@gmail.com>
2024-03-27 16:30:31 -0500

    Port change

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 const fs = require('fs')
 const {v4 : uuidv4} = require('uuid')
 
